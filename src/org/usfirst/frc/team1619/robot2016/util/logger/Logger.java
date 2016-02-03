6d019f205ad4ccbd88b6434a2424b74dc88a580f
Daniel Hathcock <danielperson7@gmail.com>
2016-02-02 20:02:42 -0700

    lololololololol this will be sqaushed

--- src/org/usfirst/frc/team1619/robot2016/util/logger/Logger.java
+++ src/org/usfirst/frc/team1619/robot2016/util/logger/Logger.java
@@ -15,6 +15,21 @@ import org.usfirst.frc.team1619.robot2016.Properties;
  */
 public class Logger extends GenericLogger {
 
+  /**
+   * Created by DanielHathcock on 10/20/15. Project: Logger Package:
+   * org.usfirst.frc.team1619.logger
+   */
+  public enum LoggingLevel {
+    ERROR, WARNING, INFO, DEBUG;
+
+    @Override
+    public String toString() {
+      return name().charAt(0) + name().substring(1).toLowerCase();
+    }
+  }
+  
+  private LoggingLevel loggingLevel; 
+  
   public Logger() {
     super("UACRRobot" + Properties.getLoggingLevel() + "Log", ".txt");
     nextLog();
