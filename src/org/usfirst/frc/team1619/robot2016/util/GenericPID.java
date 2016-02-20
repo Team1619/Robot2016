3e79e08b26bde7bdecaea9aa01e71f2106fcea3e
TimEwing <TimJEwing@gmail.com>
2016-02-19 21:24:41 -0700

    feat(genericPID): Add IMax, IRange, and fix reset

--- src/org/usfirst/frc/team1619/robot2016/util/GenericPID.java
+++ src/org/usfirst/frc/team1619/robot2016/util/GenericPID.java
@@ -3,17 +3,20 @@ package org.usfirst.frc.team1619.robot2016.util;
 import org.usfirst.frc.team1619.robot2016.PIDValues;
 
 public class GenericPID {
-  private double kP;
-  private double kI;
-  private double kD;
+  protected double kP;
+  protected double kI;
+  protected double kD;
 
-  private double outputValue;
-  private double setPoint;
-  private double prevError;
-  private double integral;
+  protected double outputValue;
+  protected double setPoint;
+  protected double prevError;
+  protected double integral;
+
+  protected double integralRange;
+  protected double integralMax;
 
   public GenericPID() {
-    this(new PIDValues(0.0, 0.0, 0.0));
+    this(0.0, 0.0, 0.0);
   }
 
   public GenericPID(PIDValues values) {
@@ -27,14 +30,34 @@ public class GenericPID {
     integral = 0;
   }
 
+  public GenericPID(double pValue, double iValue, double dValue) {
+    kP = pValue;
+    kI = iValue;
+    kD = dValue;
+
+    outputValue = 0;
+    setPoint = 0;
+    prevError = 0;
+    integral = 0;
+
+    integralRange = 0;
+    integralMax = 0;
+  }
+
   public void setValues(PIDValues values) {
     this.kP = values.p;
     this.kI = values.i;
     this.kD = values.d;
   }
 
+  public void setValues(double pValue, double iValue, double dValue) {
+    kP = pValue;
+    kI = iValue;
+    kD = dValue;
+  }
+
   public void setTarget(double target) {
-    this.setPoint = target;
+    setPoint = target;
   }
 
   public void calculate(double currentValue) {
@@ -45,8 +68,38 @@ public class GenericPID {
     return outputValue;
   }
 
+  public double getSetPoint() {
+    return setPoint;
+  }
+
   public void resetError() {
-    this.integral = 0;
+    integral = 0;
+  }
+
+  /**
+   * Reset the PID loop to initial conditions
+   */
+  public void reset() {
+    outputValue = 0;
+    setPoint = 0;
+    prevError = 0;
+    integral = 0;
+  }
+
+  /**
+   * Integral term will only be calculated when -range < error < range
+   * @param range Range that the I term is calculated within
+   */
+  public void setIRange(double range) {
+    integralRange = range;
+  }
+
+  /**
+   * Integral will be constrained to values between -max and max
+   * @param max Maximum integral value
+   */
+  public void setIMax(double max) {
+    integralMax = max;
   }
 
   private double calcPID(double error) {
@@ -60,15 +113,30 @@ public class GenericPID {
     pCalc = currentError * this.kP;
 
     // I term
-    integral += currentError;
-    iCalc = integral * this.kI;
+    //If there is an integral range, only add the integral term within bounds
+    if(integralRange != 0) {
+      if(currentError <= integralRange && currentError >= -integralRange) {
+        integral += currentError;
+      }
+      else {
+        integral = 0;
+      }
+    }
+    else {
+      integral += currentError;
+    }
+    //If there is a integral max, constrain the final result
+    if(integralMax != 0) {
+      integral = Math.max(integralMax, Math.min(-integralMax, integral));
+    }
+    iCalc = integral * kI;
 
     // D term
     dCalc = (currentError - prevError) * this.kD;
 
     // output
     output = pCalc + iCalc + dCalc;
-    this.prevError = currentError;
+    prevError = currentError;
     return output;
   }
 }
